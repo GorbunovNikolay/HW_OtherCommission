76c75276c8ccd3ebf46f8b3217e59542c3c02f36
Gorbunov Nikolay <gor_bunk@mail.ru>
2021-12-03 10:52:18 +0300

    Добавлена ошибка

--- src/test/kotlin/MainKtTestCheckingTheCalculation.kt
+++ src/test/kotlin/MainKtTestCheckingTheCalculation.kt
@@ -9,7 +9,7 @@ class MainKtTestCheckingTheCalculation {
         val myAmmountCurrentTransfer =  45000.00
         val myAmountOfPreviousTransfers = 85000.00
         val myPaymentSystem = "Visa"
-        val expectedCommission = 270.00
+        val expectedCommission = 300.00
         val actualCommission = calculateСommission(myAmmountCurrentTransfer, myAmountOfPreviousTransfers, myPaymentSystem)
         assertEquals(expectedCommission, actualCommission, 0.00)
     }
